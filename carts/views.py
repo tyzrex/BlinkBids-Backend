d336306753dc3058d439d14770c3362b9a941c23
binisha-banjara <rubiks1200@gmail.com>
2024-03-31 08:45:27 +0545

    add to cart error fix

--- carts/views.py
+++ carts/views.py
@@ -1,25 +1,35 @@
-# carts/views.py
-from rest_framework import generics, status
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Cart, CartItems
 from .serializers import CartItemsSerializer
 from product.models import Product
-from rest_framework.views import APIView
 from account.models import User
+
 
 class AddToCartView(APIView):
     def patch(self, request, *args, **kwargs):
         user = self.request.user
-        print(user)
+        if not user.is_authenticated:
+            return Response("You are not logged in", status=status.HTTP_403_FORBIDDEN)
+
         product_id = request.data.get("product_id")
+        if not product_id:
+            return Response(
+                "Product ID is required", status=status.HTTP_400_BAD_REQUEST
+            )
 
-        cart, created = Cart.objects.get_or_create(user=User)
-        if request.user.is_authenticated:
-           cart, created = Cart.objects.get_or_create(user=request.user)
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            return Response("Product not found", status=status.HTTP_404_NOT_FOUND)
 
+        cart, created = Cart.objects.get_or_create(user=user)
+        cart_item, item_created = CartItems.objects.get_or_create(
+            cart=cart, product=product, user=user
+        )
+
+        if item_created:
+            return Response("Product added to cart", status=status.HTTP_200_OK)
         else:
-            return Response("You are not logged in", status.HTTP_403_FORBIDDEN)
-        
-        cart_items = cart.items.get_or_create(product=Product)
-
-        return Response("Product added to cart", status=status.HTTP_200_OK)
+            return Response("Product already exists in cart", status=status.HTTP_200_OK)
