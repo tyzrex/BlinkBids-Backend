9c3e32ec996dd7de3d0f8d60d4c3c1a445cc56ad
binisha-banjara <rubiks1200@gmail.com>
2024-04-05 16:55:11 +0545

    implemet cart update and view cart detail functionality

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from djoser.views import TokenCreateView
 from .views import home
 
 urlpatterns = [
-    path('auth/', include ('djoser.urls')),
-    path('auth/', include ('djoser.urls.jwt')),
-    path('home/',home)
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("token/", TokenCreateView.as_view(), name="token_create"),
+    path("home/", home),
 ]
--- carts/views.py
+++ carts/views.py
@@ -17,9 +17,7 @@ class AddToCartView(APIView):
 
         product_id = request.data.get("product_id")
         if not product_id:
-            return Response(
-                "Product ID is required", status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response("Product ID is required", status=status.HTTP_400_BAD_REQUEST)
 
         try:
             product = Product.objects.get(pk=product_id)
@@ -27,9 +25,7 @@ class AddToCartView(APIView):
             return Response("Product not found", status=status.HTTP_404_NOT_FOUND)
 
         cart, created = Cart.objects.get_or_create(user=user)
-        cart_item, item_created = CartItems.objects.get_or_create(
-            cart=cart, product=product, user=user
-        )
+        cart_item, item_created = CartItems.objects.get_or_create(cart=cart, product=product, user=user)
 
         if not item_created:
             cart_item.quantity += 1
@@ -37,6 +33,77 @@ class AddToCartView(APIView):
 
         return Response("Product added to cart", status=status.HTTP_200_OK)
     
+
+# class RemoveFromCartView(APIView):
+#     def delete(self, request, *args, **kwargs):
+#         user = self.request.user
+#         if not user.is_authenticated:
+#             return Response("You are not logged in", status=status.HTTP_403_FORBIDDEN)
+
+#         product_id = request.data.get("product_id")
+#         if not product_id:
+#             return Response(
+#                 "Product ID is required", status=status.HTTP_400_BAD_REQUEST
+#             )
+
+#         try:
+#             product = Product.objects.get(pk=product_id)
+#         except Product.DoesNotExist:
+#             return Response("Product not found", status=status.HTTP_404_NOT_FOUND)
+
+#         cart_item = CartItems.objects.filter(cart__user=user, product=product).first()
+#         if not cart_item:
+#             return Response(
+#                 "Product not found in your cart", status=status.HTTP_404_NOT_FOUND
+#             )
+
+#         cart_item.delete()
+#         return Response("Product removed from cart", status=status.HTTP_200_OK)
+    
+class UpdateCartItemView(APIView):
+    def patch(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Response("You are not logged in", status=status.HTTP_403_FORBIDDEN)
+
+        product_id = request.data.get("product_id")
+        quantity = request.data.get("quantity")
+
+        if not product_id or not quantity:
+            return Response(
+                "Product ID and quantity are required",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            return Response("Product not found", status=status.HTTP_404_NOT_FOUND)
+
+        cart, _ = Cart.objects.get_or_create(user=user)
+        cart_item, created = CartItems.objects.get_or_create(
+            cart=cart, product=product, user=user
+        )
+
+        if int(quantity) <= 0:
+            cart_item.delete()
+            return Response("Product removed from cart", status=status.HTTP_200_OK)
+        else:
+            cart_item.quantity = quantity
+            cart_item.save()
+            return Response("Quantity updated in cart", status=status.HTTP_200_OK)
+
+class CartDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Response("You are not logged in", status=status.HTTP_403_FORBIDDEN)
+
+        cart_items = CartItems.objects.filter(cart__user=user)
+        serializer = CartItemsSerializer(cart_items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class CheckoutView(APIView):
     def post(self, request, *args, **kwargs):
         user = self.request.user
