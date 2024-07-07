fd55e94a0ead811ed57e2934730d3d2a3c1a9125
shalin-devkota <shalin.devkota01@gmail.com>
2024-07-07 19:39:08 +0545

    aaa

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import  Cart, CartItems
+from .models import  Cart, CartItems, Order, OrderItem
 # Register your models here.
 
 admin.site.register(Cart)
 admin.site.register(CartItems)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- carts/models.py
+++ carts/models.py
@@ -6,22 +6,40 @@ from product.models import Product
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     ordered = models.BooleanField(default=False)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
+
+    def __str__(self):
+        return self.user.email
+    
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.cart_items.all())
 
 class CartItems(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name="cart_items")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     quantity = models.IntegerField(default=1)
 
+    def __str__(self):
+        return f"{self.product.title}[{self.quantity}] - {self.user.email}"
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.order_items.all())
 
+    def __str__(self):
+        return self.user.email
+    
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name="order_items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.title}[{self.quantity}] - {self.order.user.email}"
