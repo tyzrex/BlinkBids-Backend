e6c4f39ba44a51e4a100ceffceb0fb863801cc14
shalin-devkota <shalin.devkota01@gmail.com>
2024-07-07 18:48:38 +0545

    oopsie daisy

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductList, ProductCreate, ProductUpdate, ProductDelete, CategoryList, CategoryCreate, CategoryUpdate, CategoryDelete
+from .views import ProductList, ProductCreate, ProductUpdate, ProductDelete, CategoryList, CategoryCreate, CategoryUpdate, CategoryDelete,ProductDetail
 
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
     path("create/", ProductCreate.as_view(), name="product-create"),
     path("update/<str:pk>/", ProductUpdate.as_view(), name="product-update"),
     path("delete/<str:pk>/", ProductDelete.as_view(), name="product-delete"),
-
+    path('product/<slug:slug>/', ProductDetail.as_view(), name='product-detail'),
     path("category/all/", CategoryList.as_view(), name="category-list"),
     path("category/create/", CategoryCreate.as_view(), name="category-create"),
     path("category/update/<str:pk>/", CategoryUpdate.as_view(), name="category-update"),
